94b82dc619ebadb18ad5b6d0c4963460d2f3ca97
Mengist <mengist.teshale@gmail.com>
2026-03-30 20:43:40 -0600

    Added POST, PUT, PATCH, and DELETE methods to our APIView

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -79,6 +79,14 @@ WSGI_APPLICATION = "profile_project.wsgi.application"
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
@@ -87,7 +95,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from profiles_api import views
 
 urlpatterns = [
-    path("hello-view/", views.HelloApiView.as_view()),
+path('hello-view/', views.HelloApiView.as_view()),
+path('hello-view/<int:pk>/', views.HelloApiView.as_view()),
 ]
